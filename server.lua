0da8f825c8803b8e58668cd3d55de96be1228829
Vincent Acanfora <brodino96@gmail.com>
2024-10-01 17:06:29 +0200

    Removed char1: from identifier

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ ESX = exports["es_extended"]:getSharedObject()
 
 lib.callback.register("firekeeper:init", function (source)
 
-    local playerIdentifier = ESX.GetPlayerFromId(source).getIdentifier()
+    local playerIdentifier = string.gsub(ESX.GetPlayerFromId(source).getIdentifier(), "char1:", "")
 
     for i = 1, #Identifiers do
         if Identifiers[i] == playerIdentifier then
